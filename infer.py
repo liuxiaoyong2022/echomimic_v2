69700f67aba1b981fd22853b68e94a46c522d4cf
lxy_2021 <9031848+lxy-two-hundred-and-two@user.noreply.gitee.com>
2025-01-09 20:38:57 +0800

    modify frame length

--- infer.py
+++ infer.py
@@ -177,8 +177,9 @@ def main():
     
     ref_image_pil = Image.open(inputs_dict['refimg']).resize((args.W, args.H))
     audio_clip = AudioFileClip(inputs_dict['audio'])
-    
-    # args.L = min(args.L, int(audio_clip.duration * final_fps), len(os.listdir(inputs_dict['pose'])))
+
+    args.L = min(args.L, int(audio_clip.duration * final_fps), len(os.listdir(inputs_dict['pose'])))
+    print(f"----> args.L:{args.L}" )
 
     pose_list = []
     for index in range(start_idx, start_idx + args.L):
